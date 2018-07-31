edd08a3d3ebd5df248a0d484c5737ac8e240d3b5
Saiylor <ethanimooney@gmail.com>
2018-07-31 18:40:43 -0500

    commiting the code

---
+++ mybot.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+
+const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if(command === 'ping') {
+    message.channel.send('Pong!');
+    } else
+  if (command === 'blah') {
+    message.channel.send('Meh.');
+    }
+
+});
+
+client.login(config.token);
