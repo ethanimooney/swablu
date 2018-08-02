ed7cfc26b38fb59aaac7cdda9e5ace6b841d8d1a
Saiylor <ethanimooney@gmail.com>
2018-08-01 20:23:39 -0500

    ad

--- mybot.js
+++ mybot.js
@@ -21,7 +21,7 @@ const command = args.shift().toLowerCase();
     message.channel.send('Meh nah.');
     } else 
   if(command === "announce"){
-    let channelid = args.slice(0).join(" ");
+    let channelid = args.slice(2).join(" ");
     let text = args.slice(1).join(" ");
     message.delete();
     message.guild.channels.find("name", "general").send(text, channelid);
