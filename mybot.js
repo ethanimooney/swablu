0988dadccd9d0b86fac89d3fe5144443c5ef45b5
Saiylor <ethanimooney@gmail.com>
2018-08-01 20:01:39 -0500

    dgfd

--- mybot.js
+++ mybot.js
@@ -20,11 +20,20 @@ const command = args.shift().toLowerCase();
   if (command === 'blah') {
     message.channel.send('Meh nah.');
     } else 
-  if(command === "announce"){
-    let text = args.slice(0).join(" ");
-    message.delete();
-    message.channelid("general").send(text);
-    }
+  // if(command === "announce"){
+  //   let text = args.slice(0).join(" ");
+  //   message.delete();
+  //   message.channelid("general").send(text);
+  //   }
+  if(command === "quote") {
+    const [channelid, messageid, quotename, ...note] = args.splice(1);
+    // I also support "here" as a channelID using this:
+    const channel = channelid == "here" ? message.channel : client.channels.get(channelid);
+    // I do the same with message ID, which can be "last":
+    const message = messageid === "last" ? msg.channel.messages.last(2)[0] : await channel.messages.get(messageid);
+    // pretend for a second this is the rest of the function:
+    insertInDB(quotename, channel.id, message.id, note.join(" "));
+  }
 
 });
 
