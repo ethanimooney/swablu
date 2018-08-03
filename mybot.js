d4050d994704e034409806d4fa0d29938b497042
Saiylor <ethanimooney@gmail.com>
2018-08-03 15:32:41 -0500

    changed to switch

--- mybot.js
+++ mybot.js
@@ -14,15 +14,43 @@ const command = args.shift().toLowerCase();
 
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-  if(command === "announce"){ //announces a message to a given channel
-    let channelid = args.slice(0,1).join(" "); //selects first argument, the channelid
-    let text = args.slice(1).join(" "); //selects second argument, message
-    message.delete(); //deletes command invocation message
-    message.guild.channels.find("name", channelid).send(text); //sends message in the channel of the defined channelid
-    } else
-  if(command == "hello"){
-    message.channel.send("hello");
+  // if(command === "announce"){ //announces a message to a given channel
+  //   let channelid = args.slice(0,1).join(" "); //selects first argument, the channelid
+  //   let text = args.slice(1).join(" "); //selects second argument, message
+  //   message.delete(); //deletes command invocation message
+  //   message.guild.channels.find("name", channelid).send(text); //sends message in the channel of the defined channelid
+  //   } else
+  // if(command == "hello"){
+  //   message.channel.send("hello");
+  // } else 
+  // if(command == "kick"){
+  //   const modRole = message.guild.roles.find("name", "Professors");
+
+
+  // }
+
+
+  switch(command){
+
+    case "announce":
+      let channelid = args.slice(0,1).join(" "); //selects first argument, the channelid
+      let text = args.slice(1).join(" "); //selects second argument, message
+      message.delete(); //deletes command invocation message
+      message.guild.channels.find("name", channelid).send(text); //sends message in the channel of the defined channelid
+    break;
+
+    case "hello":
+      message.channel.send("hello");
+    break;
+
+   // case "kick":
+   //   const modRole = message.guild.roles.find("name", "Professors");
+
+
+
   }
+
+  
 
 });
 
