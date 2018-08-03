c806414a4ee5302531f3df5e60f6385bb2b1c23f
Saiylor <ethanimooney@gmail.com>
2018-08-03 17:13:53 -0500

    +added more documentation to announce command

--- swablu.js
+++ swablu.js
@@ -16,7 +16,7 @@ const command = args.shift().toLowerCase();
 
   switch(command){
 
-    case "announce":
+    case "announce": //announces message specified in speified channel
       let channelid = args.slice(0,1).join(" "); //selects first argument, the channelid
       let text = args.slice(1).join(" "); //selects second argument, message
       message.delete(); //deletes command invocation message
