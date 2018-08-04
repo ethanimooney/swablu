42d81f58c7b39def8421ac7fa8abec45aca910a8
Saiylor <ethanimooney@gmail.com>
2018-08-04 10:12:26 -0500

    +added clear command

--- swablu.js
+++ swablu.js
@@ -59,10 +59,11 @@ const command = args.shift().toLowerCase();
 
     case "clear":
 
+    const swablu = client.emojis.find("name", "swablu");
    
-  if(!args[0]) return message.channel.send("oof");
+  if(!args[0]) return message.channel.send("Erm, how many? Try again.");
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+    message.channel.send(`Cleared ${args[0]} messages. ${swablu}`).then(msg => msg.delete(5000));
   });
 
 
