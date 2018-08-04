7769f59214a4b001f7f033ccd87be2215bd73f2a
Saiylor <ethanimooney@gmail.com>
2018-08-04 16:43:24 -0500

    test

--- swablu.js
+++ swablu.js
@@ -60,10 +60,12 @@ const command = args.shift().toLowerCase();
     case "clear"://clears a specified amount of messages from chat
 
     const swablu = client.emojis.find("name", "swablu");//creates swablu emoji
+
+    let clearNumber = (args[0] + 1);
    
     message.delete();//deletes command invocation message
     if(!args[0]) return message.channel.send("Erm, how many? Try again.");//if no amount to delete is given, throws error
-    message.channel.bulkDelete((args[0])).then(() => {//does the deleting
+    message.channel.bulkDelete(clearNumber).then(() => {//does the deleting
     message.channel.send(`Cleared ${args[0]} messages. ${swablu}`).then(msg => msg.delete(5000));//sends conformation, then deletes conformation
   });
 
