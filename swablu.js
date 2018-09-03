cd3d85bd72ed347f7850bcad06e31a6cb4f4748b
Saiylor <ethanimooney@gmail.com>
2018-09-03 18:45:26 -0500

    +added mod lock for clear command

--- swablu.js
+++ swablu.js
@@ -60,6 +60,14 @@ const command = args.shift().toLowerCase();
     case "clear"://clears a specified amount of messages from chat
 
     const swablu = client.emojis.find("name", "swablu");//creates swablu emoji
+    const modRole = message.guild.roles.find("name", "professors"); //finds mod role
+
+    if (!modRole){ //checks if there is a mod role in the server
+      return console.log("The Mods role does not exist");
+    }
+    if (!message.member.roles.has(modRole.id)){ //checks if command user is mod
+      return message.reply("You can't use this command.");
+    }
 
     let clearNumber = parseInt(args[0], 10);//converts number to an int
    let actualNumber = (clearNumber + 1);//sets delete number to 1 higher so it delets command invocation
